b5be26ddc83b11b6e9e1db0dbccd55204b4ee478
Shinya Takeuchi <bamboo@rj.dream.jp>
2015-05-17 11:15:36 +0900

    #1 build script change

--- doc/stkthread/stkthread.bat
+++
@@ -1,7 +0,0 @@
-del *.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale frame_r.htm stkthread00.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale section01.htm stkthread01.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale section02.htm stkthread02.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale section03.htm stkthread03.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale section05.htm stkthread05.pdf
-"..\..\build\pdftk" *.pdf output stkthread.pdf
--- doc/stkthreadgui/stkthreadgui.bat
+++
@@ -1,4 +0,0 @@
-del *.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale frame_r.htm stkthreadgui00.pdf
-"..\..\build\wkhtmltopdf.exe" --zoom 1.3 --disable-internal-links --disable-external-links --grayscale section01.htm stkthreadgui01.pdf
-"..\..\build\pdftk" *.pdf output stkthreadgui.pdf
