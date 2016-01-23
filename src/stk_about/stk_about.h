60485ec4cacdf5e4da533df532033c4289f85557
Shinya Takeuchi <bamboo@rj.dream.jp>
2016-01-24 00:53:12 +0900

    #29 Files regarding Stk_About have been deleted from YaizuComLib project.

--- src/stk_about/stk_about.h
+++
@@ -1,7 +0,0 @@
-void SetAboutTitle(TCHAR*);
-void AddHistoryItem(WORD, TCHAR*, TCHAR*, TCHAR*);
-void ShowAboutDialog(HWND);
-void ClearHistoryItem();
-void SetStkAboutIconAbout(int);
-void SetStkAboutIconBug(int);
-void SetStkAboutIconEnhance(int);
