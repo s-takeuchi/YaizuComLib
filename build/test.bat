31fae95a1e00ee2db243fbc3e79c54af5d3525a4
Shinya Takeuchi <bamboo@rj.dream.jp>
2019-02-16 22:20:42 +0900

    #99: Errorlevel processing code has been added to test script.

--- build/test.bat
+++ build/test.bat
@@ -16,32 +16,41 @@ echo;
 
 echo Building stksockettest.sln...
 %MSBUILD% "..\test\stksockettest\stksockettest.sln" /t:clean;build /p:Configuration=Release
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo Building stkdatatest.sln...
 %MSBUILD% "..\test\stkdatatest\stkdatatest.sln" /t:clean;build /p:Configuration=Release
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo Building stkthreadtest.sln...
 %MSBUILD% "..\test\stkthreadtest\stkthreadtest.sln" /t:clean;build /p:Configuration=Release
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo Building commonfunctest.sln...
 %MSBUILD% "..\test\commonfunctest\commonfunctest.sln" /t:clean;build /p:Configuration=Release
 echo Building stkwebapptest.sln...
 %MSBUILD% "..\test\stkwebapptest\stkwebapptest.sln" /t:clean;build /p:Configuration=Release
+IF %ERRORLEVEL% NEQ 0 goto testexit
 
 echo ---------------------------------------
 echo stksockettest.exe
 ..\test\stksockettest\release\\stksockettest.exe
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo ---------------------------------------
 echo stkdatatest.exe
 ..\test\stkdatatest\release\stkdatatest.exe
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo ---------------------------------------
 echo stkthreadtest.exe
 ..\test\stkthreadtest\release\stkthreadtest.exe
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo ---------------------------------------
 echo commonfunctest.exe
 ..\test\commonfunctest\release\commonfunctest.exe
+IF %ERRORLEVEL% NEQ 0 goto testexit
 echo ---------------------------------------
 echo stkwebapptest.exe
 ..\test\stkwebapptest\release\stkwebapptest.exe
+IF %ERRORLEVEL% NEQ 0 goto testexit
 
-
+:testexit
 echo;
 echo Testing libraries has done.
 echo;
@@ -49,3 +58,4 @@ echo;
 if not defined APPVEYOR (
   pause
 )
+exit /b %ERRORLEVEL%
--- build/test.sh
+++ build/test.sh
@@ -1,43 +1,71 @@
-#!/bin/sh
+#! /bin/bash
 
 ########## Testing libraries ##########
+echo
 echo ==========================================
 echo Testing libraries
+echo
 
 cd ../test
 
 echo Building commonfunctest
 cd commonfunctest
 make all
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 cd ..
 
 echo Building stksockettest...
 cd stksockettest
 make all
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 cd ..
 
 echo Building stkthreadtest...
 cd stkthreadtest
 make all
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 cd ..
 
 echo Building stkwebapptest
 cd stkwebapptest
 make all
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 cd ..
 
 echo ---------------------------------------
 echo commonfunctest
 ./commonfunctest/commonfunctest
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 echo ---------------------------------------
 echo stksockettest
 ./stksockettest/stksockettest
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 echo ---------------------------------------
 echo stkthreadtest
 ./stkthreadtest/stkthreadtest
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 echo ---------------------------------------
 echo stkwebapptest
 ./stkwebapptest/stkwebapptest
+if [ $? -ne 0 ]; then
+  exit $?
+fi
 
+echo
 echo Testing libraries has done.
+echo
 
