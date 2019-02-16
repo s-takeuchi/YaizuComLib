dcfc2320945163368cf366b37d45e1c67737951c
Shinya Takeuchi <s-takeuchi@users.noreply.github.com>
2019-02-16 09:18:33 +0900

    #99: build.sh has been modified.

--- build/build.sh
+++ build/build.sh
@@ -1,9 +1,43 @@
 #!/bin/sh
 
-echo hello
-ls /opt/rh
-cd ../test/commonfunctest/
-make all
-cd ../../build
+########## Testing libraries ##########
+echo ==========================================
+echo Testing libraries
 
+cd ../test
+
+echo Building commonfunctest
+cd commonfunctest
+make all
+cd ..
+
+echo Building stksockettest...
+cd stksockettest
+make all
+cd ..
+
+echo Building stkthreadtest...
+cd stkthreadtest
+make all
+cd ..
+
+echo Building stkwebapptest
+cd stkwebapptest
+make all
+cd ..
+
+echo ---------------------------------------
+echo commonfunctest
+./commonfunctest/commonfunctest
+echo ---------------------------------------
+echo stksockettest
+./stksockettest/stksockettest
+echo ---------------------------------------
+echo stkthreadtest
+./stkthreadtest/stkthreadtest
+echo ---------------------------------------
+echo stkwebapptest
+./stkwebapptest/stkwebapptest
+
+echo Testing libraries has done.
 
