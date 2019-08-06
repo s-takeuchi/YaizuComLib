5d620664fd2061b97965d330ad837885a2546dc8
Shinya Takeuchi <bamboo@rj.dream.jp>
2019-08-07 00:51:34 +0900

    #110 : Test code

--- test/stkwebapptest/StkWebAppTest4.cpp
+++ test/stkwebapptest/StkWebAppTest4.cpp
@@ -4,6 +4,10 @@
 
 StkObject* StkWebAppTest4::Execute(StkObject* ReqObj, int Method, wchar_t UrlPath[StkWebAppExec::URL_PATH_LENGTH], int* ResultCode, wchar_t* HttpHeader)
 {
+	if (StkPlWcsStr(HttpHeader, L"GET") == 0 || StkPlWcsStr(HttpHeader, L"Content-Length:") == 0 || StkPlWcsStr(HttpHeader, L"Content-Type:") == 0) {
+		*ResultCode = -1;
+		return NULL;
+	}
 	wchar_t Buf[10];
 	StkStringParser::ParseInto1Param(UrlPath, L"/aaa/$/", L'$', Buf, 10);
 	*ResultCode = StkPlWcsToL(Buf);
