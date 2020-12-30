ab5f2b58102c435fac6e0ed14dc4e86c46cba2dd
Shinya Takeuchi <sy-takeuchi@outlook.com>
2020-12-30 23:53:42 +0900

    #135 : Message ID changes

--- src/stkwebapp_um/stkcommon_um.js
+++ src/stkwebapp_um/stkcommon_um.js
@@ -4,26 +4,25 @@ var selectedUserId = -1;
 // User operation result
 var userOpeStatus = 0;
 
-function initClientMessage4Um() {
+{
     //
     // User information
     //
-    addClientMessage('USERMGMT', {'en':'User Management', 'ja':'ユーザー管理'});
-    addClientMessage('USERMGMTS', {'en':'User Mgmt', 'ja':'ユーザー管理'});
-    addClientMessage('NOUSEREXIST', {'en':'<p>No user exists</p>', 'ja':'<p>ユーザーは存在しません</p>'});
-    addClientMessage('USERNAME', {'en':'User Name', 'ja':'ユーザー名'});
-    addClientMessage('USERROLE', {'en':'User Role', 'ja':'ユーザーロール'});
-    addClientMessage('USERROLEADMIN', {'en':'Administrator', 'ja':'管理者'});
-    addClientMessage('USERROLEUSER', {'en':'General User', 'ja':'一般ユーザー'});
-    addClientMessage('USEROPECOMPLETED', {'en':'The operation has completed.', 'ja':'操作が完了しました。'});
-    addClientMessage('USER_PASSWORD_ON', {'en':'Configure password', 'ja':'パスワードを設定する'});
-    addClientMessage('USER_PASSWORD', {'en':'Password', 'ja':'パスワード'});
-    addClientMessage('USER_CHG_PW', {'en':'Change Password', 'ja':'パスワードの変更'});
-    addClientMessage('USER_CHG_PW_CURRENT', {'en':'Current Password', 'ja':'現在のパスワード'});
-    addClientMessage('USER_CHG_PW_NEW', {'en':'New Password', 'ja':'新しいパスワード'});
-    addClientMessage('USER_CHG_PW_CONFIRM', {'en':'Confirm New Password', 'ja':'新しいパスワード(確認用)'});
-    addClientMessage('USER_PW_WRONG', {'en':'The specified password is not correct.', 'ja':'指定されたパスワードが不正です。'});
-    addClientMessage('USER_NEWPW_WRONG', {'en':'The specified "New Password" and "Confirm New Password" are not matched.', 'ja':'"新しいパスワード"と"新しいパスワード(確認用)"が不一致です。'});
+    addClientMessage('STKCOMMONUG_USERMGMT', {'en':'User Management', 'ja':'ユーザー管理'});
+    addClientMessage('STKCOMMONUG_NOUSEREXIST', {'en':'<p>No user exists</p>', 'ja':'<p>ユーザーは存在しません</p>'});
+    addClientMessage('STKCOMMONUG_USERNAME', {'en':'User Name', 'ja':'ユーザー名'});
+    addClientMessage('STKCOMMONUG_USERROLE', {'en':'User Role', 'ja':'ユーザーロール'});
+    addClientMessage('STKCOMMONUG_USERROLEADMIN', {'en':'Administrator', 'ja':'管理者'});
+    addClientMessage('STKCOMMONUG_USERROLEUSER', {'en':'General User', 'ja':'一般ユーザー'});
+    addClientMessage('STKCOMMONUG_USEROPECOMPLETED', {'en':'The operation has completed.', 'ja':'操作が完了しました。'});
+    addClientMessage('STKCOMMONUG_USER_PASSWORD_ON', {'en':'Configure password', 'ja':'パスワードを設定する'});
+    addClientMessage('STKCOMMONUG_USER_PASSWORD', {'en':'Password', 'ja':'パスワード'});
+    addClientMessage('STKCOMMONUG_USER_CHG_PW', {'en':'Change Password', 'ja':'パスワードの変更'});
+    addClientMessage('STKCOMMONUG_USER_CHG_PW_CURRENT', {'en':'Current Password', 'ja':'現在のパスワード'});
+    addClientMessage('STKCOMMONUG_USER_CHG_PW_NEW', {'en':'New Password', 'ja':'新しいパスワード'});
+    addClientMessage('STKCOMMONUG_USER_CHG_PW_CONFIRM', {'en':'Confirm New Password', 'ja':'新しいパスワード(確認用)'});
+    addClientMessage('STKCOMMONUG_USER_PW_WRONG', {'en':'The specified password is not correct.', 'ja':'指定されたパスワードが不正です。'});
+    addClientMessage('STKCOMMONUG_USER_NEWPW_WRONG', {'en':'The specified "New Password" and "Confirm New Password" are not matched.', 'ja':'"新しいパスワード"と"新しいパスワード(確認用)"が不一致です。'});
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -39,7 +38,7 @@ function transDisplayUser() {
 
 function displayUser() {
     var userMgmt = $('<div id="usermgmt">');
-    showInputModal(getClientMessage('USERMGMT'), userMgmt);
+    showInputModal(getClientMessage('STKCOMMONUG_USERMGMT'), userMgmt);
 
     if (statusCode['API_GET_USERS'] == -1 || statusCode['API_GET_USERS'] == 0) {
         displayAlertDanger('#usermgmt', getClientMessage('CONNERR'));
@@ -52,7 +51,7 @@ function displayUser() {
     }
     var userList = getArray(responseData['API_GET_USERS'].Data.User);
     if (userList == null) {
-        $('#usermgmt').append(getClientMessage('NOUSEREXIST'));
+        $('#usermgmt').append(getClientMessage('STKCOMMONUG_NOUSEREXIST'));
     }
 
     selectedUserId = -1;
@@ -62,16 +61,16 @@ function displayUser() {
         userListTable.addClass('table table-striped');
 
         var tHead = $('<thead class="thead-dark">');
-        tHead.append('<tr><th>' + getClientMessage('USERNAME') + '</th><th>' + getClientMessage('USERROLE') + '</th></tr>');
+        tHead.append('<tr><th>' + getClientMessage('STKCOMMONUG_USERNAME') + '</th><th>' + getClientMessage('STKCOMMONUG_USERROLE') + '</th></tr>');
         userListTable.append(tHead);
 
         var tBody = $('<tbody>');
         for (var Loop = 0; Loop < userList.length; Loop++) {
             var StrUserRole = '';
             if (userList[Loop].Role == 0) {
-                StrUserRole = getClientMessage('USERROLEADMIN');
+                StrUserRole = getClientMessage('STKCOMMONUG_USERROLEADMIN');
             } else {
-                StrUserRole = getClientMessage('USERROLEUSER');
+                StrUserRole = getClientMessage('STKCOMMONUG_USERROLEUSER');
             }
             tBody.append('<tr><td><div class="radio"><label><input type="radio" id="radioUser' + userList[Loop].Id + '" name="optradio" onclick="selectUser(\''+ userList[Loop].Id + '\')"/>&nbsp;' + userList[Loop].Name + '</label></div></td><td>' + StrUserRole + '</td></tr>');
         }
@@ -79,17 +78,17 @@ function displayUser() {
         $('#usermgmt').append(userListTable);
     }
     $('#usermgmt').append('<div id="usermgmt-ope" style="margin-top:30px;padding-left:30px;padding-right:30px;padding-top:30px;padding-bottom:30px;border-color:#000000;border-style:solid;border-width:2px">');
-    $('#usermgmt-ope').append('<div class="form-group"><label for="userName">' + getClientMessage('USERNAME') + '</label><input type="text" class="form-control" id="userName" placeholder="' + getClientMessage('USERNAME') + '"/></div>');
-    $('#usermgmt-ope').append('<div class="form-group"><label for="userType">' + getClientMessage('USERROLE') + '</label><select class="form-control" id="userRole"><option>' + getClientMessage('USERROLEADMIN') + '</option><option>' + getClientMessage('USERROLEUSER') + '</option></select></div>');
+    $('#usermgmt-ope').append('<div class="form-group"><label for="userName">' + getClientMessage('STKCOMMONUG_USERNAME') + '</label><input type="text" class="form-control" id="userName" placeholder="' + getClientMessage('STKCOMMONUG_USERNAME') + '"/></div>');
+    $('#usermgmt-ope').append('<div class="form-group"><label for="userType">' + getClientMessage('STKCOMMONUG_USERROLE') + '</label><select class="form-control" id="userRole"><option>' + getClientMessage('STKCOMMONUG_USERROLEADMIN') + '</option><option>' + getClientMessage('STKCOMMONUG_USERROLEUSER') + '</option></select></div>');
 
     var userPwOn = '';
-    $('#usermgmt-ope').append($('<div class="form-check"><input class="form-check-input" type="checkbox" id="userPwOn" onClick="clickUserPwOn()" ' + userPwOn + '><label class="form-check-label" for="userPwOn">' + getClientMessage('USER_PASSWORD_ON') + '</label><input type="password" class="form-control" id="userPassword" placeholder="' + getClientMessage('USER_PASSWORD') + '" disabled/></div>'));
+    $('#usermgmt-ope').append($('<div class="form-check"><input class="form-check-input" type="checkbox" id="userPwOn" onClick="clickUserPwOn()" ' + userPwOn + '><label class="form-check-label" for="userPwOn">' + getClientMessage('STKCOMMONUG_USER_PASSWORD_ON') + '</label><input type="password" class="form-control" id="userPassword" placeholder="' + getClientMessage('STKCOMMONUG_USER_PASSWORD') + '" disabled/></div>'));
 
     $('#usermgmt').append('<br/>');
     $('#usermgmt').append('<div id="usermgt_msg"/>');
     if (userOpeStatus == 0) {
     } else if (userOpeStatus == 1) {
-        displayAlertSuccess('#usermgt_msg', getClientMessage('USEROPECOMPLETED'));
+        displayAlertSuccess('#usermgt_msg', getClientMessage('STKCOMMONUG_USEROPECOMPLETED'));
     } else {
         displayAlertDanger('#usermgt_msg', getSvrMsg(responseData['API_OPE_USER']));
     }
@@ -115,9 +114,9 @@ function updateUser(opeFlag) {
     var specifiedUserRole = $('#userRole').val();
     var specifiedUserPassword = $('#userPassword').val().replace(/[\n\r]/g, '');
     var tmpRole = -1;
-    if (specifiedUserRole === getClientMessage('USERROLEADMIN')) {
+    if (specifiedUserRole === getClientMessage('STKCOMMONUG_USERROLEADMIN')) {
         tmpRole = 0;
-    } else if (specifiedUserRole === getClientMessage('USERROLEUSER')) {
+    } else if (specifiedUserRole === getClientMessage('STKCOMMONUG_USERROLEUSER')) {
         tmpRole = 1;
     } else {
         closeInputModal();
@@ -157,9 +156,9 @@ function selectUser(userId) {
             selectedUserId = userList[loop].Id;
             var roleStr = '';
             if (userList[loop].Role == 0) {
-                roleStr = getClientMessage('USERROLEADMIN');
+                roleStr = getClientMessage('STKCOMMONUG_USERROLEADMIN');
             } else {
-                roleStr = getClientMessage('USERROLEUSER');
+                roleStr = getClientMessage('STKCOMMONUG_USERROLEUSER');
             }
             $('#userName').val(userList[loop].Name);
             $('#userRole').val(roleStr);
@@ -187,7 +186,7 @@ function transDisplayChgPassword() {
 
 function displayChgPassword() {
     var userMgmt = $('<div id="chgpassword">');
-    showInputModal(getClientMessage('USER_CHG_PW'), userMgmt);
+    showInputModal(getClientMessage('STKCOMMONUG_USER_CHG_PW'), userMgmt);
 
     if (statusCode['API_GET_USER'] == -1 || statusCode['API_GET_USER'] == 0) {
         displayAlertDanger('#chgpassword', getClientMessage('CONNERR'));
@@ -199,9 +198,9 @@ function displayChgPassword() {
         return;
     }
 
-    userMgmt.append('<div class="form-group"><label for="currentPw">' + getClientMessage('USER_CHG_PW_CURRENT') + '</label><input type="password" class="form-control" id="currentPw" placeholder="' + getClientMessage('USER_CHG_PW_CURRENT') + '"/></div>');
-    userMgmt.append('<div class="form-group"><label for="newPw">' + getClientMessage('USER_CHG_PW_NEW') + '</label><input type="password" class="form-control" id="newPw" placeholder="' + getClientMessage('USER_CHG_PW_NEW') + '"/></div>');
-    userMgmt.append('<div class="form-group"><label for="confirmNewPw">' + getClientMessage('USER_CHG_PW_CONFIRM') + '</label><input type="password" class="form-control" id="confirmNewPw" placeholder="' + getClientMessage('USER_CHG_PW_CONFIRM') + '"/></div>');
+    userMgmt.append('<div class="form-group"><label for="currentPw">' + getClientMessage('STKCOMMONUG_USER_CHG_PW_CURRENT') + '</label><input type="password" class="form-control" id="currentPw" placeholder="' + getClientMessage('STKCOMMONUG_USER_CHG_PW_CURRENT') + '"/></div>');
+    userMgmt.append('<div class="form-group"><label for="newPw">' + getClientMessage('STKCOMMONUG_USER_CHG_PW_NEW') + '</label><input type="password" class="form-control" id="newPw" placeholder="' + getClientMessage('STKCOMMONUG_USER_CHG_PW_NEW') + '"/></div>');
+    userMgmt.append('<div class="form-group"><label for="confirmNewPw">' + getClientMessage('STKCOMMONUG_USER_CHG_PW_CONFIRM') + '</label><input type="password" class="form-control" id="confirmNewPw" placeholder="' + getClientMessage('STKCOMMONUG_USER_CHG_PW_CONFIRM') + '"/></div>');
 
     userMgmt.append('<br/>');
     userMgmt.append('<div id="chgpassword_msg"/>');
@@ -216,11 +215,11 @@ function chgPwUpdate() {
     var specifiedConfirmNewPw = $('#confirmNewPw').val().replace(/[\n\r]/g, '');
 
     if (!isPasswordCorrect(specifiedCurrentPw)) {
-        displayAlertDanger('#chgpassword_msg', getClientMessage('USER_PW_WRONG'));
+        displayAlertDanger('#chgpassword_msg', getClientMessage('STKCOMMONUG_USER_PW_WRONG'));
         return;
     }
     if (specifiedNewPw !== specifiedConfirmNewPw) {
-        displayAlertDanger('#chgpassword_msg', getClientMessage('USER_NEWPW_WRONG'));
+        displayAlertDanger('#chgpassword_msg', getClientMessage('STKCOMMONUG_USER_NEWPW_WRONG'));
         return;
     }
 
@@ -231,7 +230,7 @@ function chgPwUpdate() {
 
 function chgPwFinal() {
     if (statusCode['API_OPE_USER'] == 200) {
-        displayAlertSuccess('#chgpassword_msg', getClientMessage('USEROPECOMPLETED'));
+        displayAlertSuccess('#chgpassword_msg', getClientMessage('STKCOMMONUG_USEROPECOMPLETED'));
         var specifiedNewPw = $('#newPw').val().replace(/[\n\r]/g, '');
         changeLoginPassword(specifiedNewPw);
         setAuthenticationToken(getUserName() + ' ' + specifiedNewPw);
