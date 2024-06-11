8d3a6fd87bd4ce51072d951bdce89314d12af44d
Shinya Takeuchi <sy-takeuchi@outlook.com>
2024-06-11 22:12:24 +0900

    #165 : Fix a description of build command usage

--- build/build.bat
+++ build/build.bat
@@ -30,7 +30,7 @@ set LCOUNTER="C:\Program Files (x86)\lcounter\lcounter.exe"
 
 echo;
 echo This batch file requires softwares shown below.
-echo 1. Microsoft Visual Studio 2017
+echo 1. Microsoft Visual Studio 2022
 echo 2. wkhtmltopdf 0.12.0.3
 echo 3. PDFtk Server 2.02
 echo 4. 7-Zip 9.20
